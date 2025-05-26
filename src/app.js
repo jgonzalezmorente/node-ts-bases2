ca44b20bfffb7796c8b07ab3ebd0688a07b59a30
jgonzalezmorente <jgonzalezmorente@gmail.com>
2025-05-26 18:32:59 +0200

    Principios Node

---
+++ src/app.js
@@ -0,0 +1,19 @@
+// const { emailTemplate } = require('./js-foundation/01-template');
+// require('./js-foundation/02-destructuring');
+// const { getUserById } = require('./js-foundation/04-arrows');
+// const { getAge, getUUID } = require('./plugins');
+// const { buildMakePerson } = require('./js-foundation/05-factory');
+const getPokemonById = require('./js-foundation/06-promises');
+
+const info = getPokemonById(3)
+    .then(pokemon => console.log({pokemon}))
+    .catch((err) => console.log(err))
+    .finally(() => console.log('Finalmente'));
+
+// ! Referencia a la función factory y uso
+// const makePerson = buildMakePerson({ getUUID, getAge });
+// const obj = { name: 'John', birthdate: '1985-10-21' };
+// const john = makePerson(obj);
+// console.log({ john });
+
+
