21a665a2140041692c954033290b52c1d588ceba
jgonzalezmorente <jgonzalezmorente@gmail.com>
2025-06-24 07:41:43 +0200

    Logger winston

--- src/app.js
+++ src/app.js
@@ -3,12 +3,17 @@
 // const { getUserById } = require('./js-foundation/04-arrows');
 // const { getAge, getUUID } = require('./plugins');
 // const { buildMakePerson } = require('./js-foundation/05-factory');
-const getPokemonById = require('./js-foundation/06-promises');
+// const getPokemonById = require('./js-foundation/06-promises');
+const { buildLogger } = require('./plugins');
 
-const info = getPokemonById(3)
-    .then(pokemon => console.log({pokemon}))
-    .catch((err) => console.log(err))
-    .finally(() => console.log('Finalmente'));
+const logger = buildLogger('app.js');
+logger.log('Hola Mundo');
+logger.error('Esto es algo malo');
+
+// const info = getPokemonById(3)
+//     .then(pokemon => console.log({pokemon}))
+//     .catch((err) => console.error(err))
+//     .finally(() => console.log('Finalmente'));
 
 // ! Referencia a la función factory y uso
 // const makePerson = buildMakePerson({ getUUID, getAge });
--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,9 +1,11 @@
 const { getUUID } = require('./get-id.plugin');
 const { getAge } = require('./get-age.plugin');
 const { http } = require('./http-client.plugin');
+const buildLogger = require('./logger.plugin');
 
 module.exports = {
     getUUID,
     getAge,
     http,
+    buildLogger
 }
